a59873125a83c6dbfa3c12a49674d592f35fd0ce
matsuokashinnya <c0b221355e@edu.teu.ac.jp>
2023-11-21 16:57:34 +0900

    演習課題2

--- pygame_image.py
+++ pygame_image.py
@@ -6,6 +6,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg") #練習1：背景画像の読み込み
+    bg_imgb = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_imgs =[kk_img, pg.transform.rotozoom(kk_img, 2, 1.0),
@@ -21,13 +22,15 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = tmr%1600
+        x = tmr%3200
+        print(x)
         screen.blit(bg_img, [-x, 0]) #練習4：背景画像の表示
-        screen.blit(bg_img, [1600-x, 0])
+        screen.blit(bg_imgb, [1600-x, 0])
+        screen.blit(bg_img, [3200-x, 0])
         screen.blit(kk_imgs[tmr%10], [300, 200])
         pg.display.update()
-        tmr += 1        
-        clock.tick(100)
+        tmr += 1
+        clock.tick(300)
 
 if __name__ == "__main__":
     pg.init()
